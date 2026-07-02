5d853bccfcdf563cc04f709fd16044bf8fa19377
Joaquim Silva <joaquimsilva96@outlook.pt>
2026-07-02 20:18:51 +0100

    chore: initialize user management api solution

---
+++ UserManagement.Tests/ApplicationSmokeTests.cs
@@ -0,0 +1,10 @@
+﻿namespace UserManagement.Tests;
+
+public sealed class ApplicationSmokeTests
+{
+    [Fact]
+    public void TestInfrastructure_IsConfigured()
+    {
+        Assert.True(true);
+    }
+}
